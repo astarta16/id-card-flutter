270e76e30ca88b74b8d0e6e645b4fd762e6f8d4d
astarta16 <ngedeshuri2001@gmail.com>
2023-11-14 17:01:21 +0400

    add image information

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: CardMain(),
-));
+      home: CardMain(),
+    ));
 
 class CardMain extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,73 +12,79 @@ class CardMain extends StatelessWidget {
       appBar: AppBar(
         title: Text("ASTARTA's ID"),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 47, 46, 46),
+        backgroundColor: Color.fromARGB(255, 47, 46, 46),
         elevation: 0.0,
       ),
-      body: Padding( 
+      body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column( 
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget> [
-            Text( 
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 80.0,
+                backgroundColor: Colors.grey,
+                backgroundImage: NetworkImage(
+                  'https://scontent.ftbs2-2.fna.fbcdn.net/v/t39.30808-6/225560109_1074175199776219_8123171032433289508_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=T6WD_KZvq04AX8vn4J5&_nc_ht=scontent.ftbs2-2.fna&oh=00_AfAIn7nYf-U57NQN_V0DmBSS_4Y8qwnvTxR4U3WLEJVUvw&oe=6558DD53',
+                ),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            Text(
               "NAME",
-              style: TextStyle(  
+              style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
                 fontSize: 30.0,
               ),
             ),
             SizedBox(height: 12.0),
-             Text( 
+            Text(
               "Nini Gedeshuri",
-              style: TextStyle(  
+              style: TextStyle(
                 color: Colors.red,
                 letterSpacing: 2.0,
                 fontSize: 20.0,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 30.0),
-             Text( 
+            Text(
               "Developer",
-              style: TextStyle(  
+              style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
                 fontSize: 30.0,
               ),
             ),
             SizedBox(height: 12.0),
-             Text( 
+            Text(
               "1.5 Years experience",
-              style: TextStyle(  
+              style: TextStyle(
                 color: Colors.red,
                 letterSpacing: 2.0,
                 fontSize: 20.0,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
               ),
             ),
-              SizedBox(height: 30.0),
-              Row(  
-                children: <Widget>[
-                  Icon( 
-                    Icons.email,
-                    color: Colors.grey,
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "ngedeshuri2001@gmail.com",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
                   ),
-                  SizedBox(width:10.0 ),
-                  Text(
-                    "ngedeshuri2001@gmail.com",
-                    style: TextStyle(  
-                      color: Colors.red,
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-
-                    ),
-                  ),
-                ],
-              )
-
-
-
+                ),
+              ],
+            ),
           ],
         ),
       ),
