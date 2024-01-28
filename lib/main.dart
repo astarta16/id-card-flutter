a83efc3e773d1598e74d8a7b9b637a4de853ba4a
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-01-28 23:39:38 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _CardMainState extends State<CardMain> {
             SizedBox(height: 12.0),
             _buildInfoText("Nini Gedeshuri", Colors.red, 20.0, true),
             SizedBox(height: 30.0),
-            _buildInfoText("Rate Developer", Colors.grey, 30.0),
+            _buildInfoText("Rate Developer skill", Colors.grey, 30.0),
             SizedBox(height: 12.0),
             _buildInfoText("$experienceYear", Colors.red, 20.0, true),
             SizedBox(height: 30.0),
